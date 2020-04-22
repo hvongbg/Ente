7c5a22e3c4970e0566ff7f94e72b063bc3b9d414
hvongbg <54710566+hvongbg@users.noreply.github.com>
2020-04-22 11:08:13 +0200

    Update server.R

--- server.R
+++ server.R
@@ -115,10 +115,10 @@ shinyServer(
       
       else{
         p<- p +
-          annotate("rect", xmin=c(1), xmax= as.numeric(input$testAk),
+          annotate("rect", xmin=c(1), xmax= as.numeric(input$testAk)+3,
                    ymin=c(1) , ymax=c(9), alpha=0.2, color="lightsalmon3", fill="lightsalmon3") +
           annotate("rect", xmin=c(1), xmax= c(9),
-                   ymin=c(1) , ymax= as.numeric(input$testBk), alpha=0.2, color="lightsalmon3", fill="lightsalmon3")
+                   ymin=c(1) , ymax= as.numeric(input$testBk)+3, alpha=0.2, color="lightsalmon3", fill="lightsalmon3")
       }
       
       p  +
