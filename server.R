4aeaca17c730ec2e71fcd799ec82f546d0a282d2
hvongbg <54710566+hvongbg@users.noreply.github.com>
2020-04-22 11:09:58 +0200

    Update server.R

--- server.R
+++ server.R
@@ -114,11 +114,14 @@ shinyServer(
       }
       
       else{
+        
+        neuA <- input$testAk +3
+        neuB <- input$testBk +3
         p<- p +
-          annotate("rect", xmin=c(1), xmax= as.numeric(input$testAk)+3,
+          annotate("rect", xmin=c(1), xmax= as.numeric(input$testAk),
                    ymin=c(1) , ymax=c(9), alpha=0.2, color="lightsalmon3", fill="lightsalmon3") +
           annotate("rect", xmin=c(1), xmax= c(9),
-                   ymin=c(1) , ymax= as.numeric(input$testBk)+3, alpha=0.2, color="lightsalmon3", fill="lightsalmon3")
+                   ymin=c(1) , ymax= as.numeric(input$testBk), alpha=0.2, color="lightsalmon3", fill="lightsalmon3")
       }
       
       p  +
